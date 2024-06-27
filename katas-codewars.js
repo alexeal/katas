06d56bf411e0fcc10a7419fe1a8731363a35259d
alexeal <chillncode@gmail.com>
2024-06-27 16:48:23 +0200

    New kata

--- katas-codewars.js
+++ katas-codewars.js
@@ -439,3 +439,16 @@ function formatDuration (seconds) {
   }
   return readableDate.length > 1 ? readableDate.join(', ').replace(/, ([^,]+)$/, ' and $1') : readableDate[0];
 }
+
+/* ************************** JADEN CASING STRINGS ************************** */
+/*  Capitalizing every word as Jaden Smith would do                           */
+/*  Not Jaden-Cased: "How can mirrors be real if our eyes aren't real".       */
+/*  Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real".       */
+/* ************************************************************************** */
+String.prototype.toJadenCase = function () {
+  return this.replace(/(^|\s)[a-z]/g, function(x){ return x.toUpperCase(); });
+};
+// Other solution
+String.prototype.toJadenCase = function() {
+  return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+};
